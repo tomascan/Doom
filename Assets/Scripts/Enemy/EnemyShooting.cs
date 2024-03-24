0da5921fc732e055777b5455b9ca6ab7b957425d
Tomás <tomascan99@gmail.com>
2024-03-24 06:01:26 +0100

    LastSoul v2
    LastSoul completamente funcional, mejor dejarlo estático o con movimientos muy lentos.

--- Assets/Scripts/Enemy/EnemyShooting.cs
+++ Assets/Scripts/Enemy/EnemyShooting.cs
@@ -4,32 +4,39 @@ public class EnemyShooting : MonoBehaviour
 {
     public GameObject bulletPrefab; // Prefab de la bala que se va a disparar
     public Transform firePoint; // El punto desde donde se dispara la bala
+    public Transform target; // Esto debería ser el jugador
     public float fireRate = 1f; // Balas por segundo
     private float nextTimeToFire = 0f;
 
     void Update()
     {
-        // Considera añadir una condición para comprobar si el jugador está en rango
+        // Comprueba si es el momento de disparar nuevamente según la cadencia de fuego
         if (Time.time >= nextTimeToFire)
         {
             nextTimeToFire = Time.time + 1f / fireRate;
-            Shoot();
+            AimAndShoot();
         }
     }
 
-    void Shoot()
+    void AimAndShoot()
     {
-        // Crear una nueva bala desde el prefab
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        
-        // Aquí puedes añadir más lógica, por ejemplo para darle una velocidad inicial a la bala
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        if (rb != null)
+        if (target != null)
         {
-            rb.velocity = transform.forward * 20f; // Reemplaza 20f con la velocidad que desees
+            // Ajusta la orientación del firePoint para que mire hacia el jugador
+            firePoint.LookAt(target.position);
+
+            // Crea la bala en el firePoint ya orientado
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                // Lanza la bala hacia adelante con una velocidad determinada
+                rb.velocity = firePoint.forward * 30f; // Ajusta la velocidad según necesites
+            }
+
+            // Opcionalmente, destruye la bala después de un tiempo para limpiar la escena
+            Destroy(bullet, 2f); // Ajusta este tiempo según necesites
         }
-        
-        // Opcionalmente, puedes destruir la bala después de un tiempo si no choca con nada
-        Destroy(bullet, 2f); // Reemplaza 5f con la cantidad de segundos antes de destruir la bala automáticamente
     }
 }
